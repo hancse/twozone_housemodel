883a99c425c5c9355702f80656f294997866b929
pjmvankan <pjmvankan@freedom.nl>
2021-04-07 15:31:29 +0200

    Added batch file for setting up environment

---
+++ conda_install.bat
@@ -0,0 +1,11 @@
+call conda create -n test python=3.8
+call activate test
+call conda install matplotlib
+call conda install scipy
+call conda install jupyter notebook
+call conda install pandas
+call conda install openpyxl
+call conda install pyyaml
+call conda install holidays
+call conda list
+call conda info -e
