dd0b2c8e80fd288d1ce4f4b668650a07c9af3ce5
pjmvankan <pjmvankan@freedom.nl>
2021-07-01 19:34:46 +0200

    Finished boilers.py (class GasBoiler) with hysteresis

--- new_yaml_xl/boilers.py
+++ new_yaml_xl/boilers.py
@@ -1,31 +1,24 @@
-
+"""
+See Also
+    https://pypi.org/project/simple-pid/
+    https://simple-pid.readthedocs.io/en/latest/
+"""
 
 import numpy as np
 # https://simple-pid.readthedocs.io/en/latest/simple_pid.html#module-simple_pid.PID
 from simple_pid import PID
-import time
+# import time
 import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 # from Temperature_SP import simple_thermostat
 
 
-class gasboiler(PID):
-    """class for modelling PID-controlled gasboiler
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
-
-    Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
+class GasBoiler(PID):
+    """class for modelling PID-controlled gas boiler.
 
     """
+
     def __init__(self, kp, ki, kd, T_setpoint,
                  T_node, T_amb, dead_band, P_max, P_min):
         """
@@ -36,8 +29,8 @@ class gasboiler(PID):
             kd (float):          derivative PID parameter
             T_setpoint (float):  setpoint (SP) temperature
             T_node (float):      process value (PV) temperature
-            T_amb (float):       outdoor temperature
-            dead_band (float):   deadband in [C]
+            T_amb (float):       outdoor temperature used for outdoor
+            dead_band (float):   dead band in [C]
             P_max (float):       maximum power of boiler [W]
             P_min (float):       minimum power of boiler [W]
         """
@@ -50,14 +43,15 @@ class gasboiler(PID):
         self.T_node = T_node
         """str: Docstring *after* attribute, with type specified."""
         self.setpoint = T_setpoint
-        self.dead_band = dead_band
+        # self.dead_band = dead_band
 
         self.lower_db = self.setpoint - 0.5 * dead_band
         self.upper_db = self.setpoint + 0.5 * dead_band
-        #self.output_limits(0, P_max)
+        # self.output_limits(0, P_max)
         self.Power = 0.0
-        self.enter_from_below = True
+        self.isEnabled: bool = True
         self.output = 0.0
+        self.current_step = 0.0
 
     """
     Todo: 
@@ -71,53 +65,59 @@ class gasboiler(PID):
     """
 
     def update(self):
-        self.output = self.__call__(self.T_node) # should happen always
+        self.output = self.__call__(self.T_node, self.current_step)  # should happen always
 
-        # If the hysteresis is upward, and within the control band, send minimal power to the room
-        if (self.lower_db <= self.T_node <= self.upper_db) & (self.enter_from_below == True):
+        # If temperature is above dead band once, disable the PID boiler, zero power
+        if self.T_node > self.upper_db:
+            if self.isEnabled:
+                self.isEnabled = False
+            self.output = 0  # overrule P_min
+
+        # If the temperature has decreased from above into the dead band, zero power
+        elif (self.lower_db <= self.T_node <= self.upper_db) & (self.isEnabled is False):
+            # self.set_auto_mode(False)
+            self.output = 0  # overrule P_min
+
+        # If the temperature has increased from below into the dead band, send minimal power
+        elif (self.lower_db <= self.T_node <= self.upper_db) & (self.isEnabled is True):
             if self.auto_mode:
                 self.set_auto_mode(False)
             self.output = self.P_min
-        # If the hysteresis is downward, and within the control band, send no power
-        elif (self.lower_db <= self.T_node <= self.upper_db) & (self.enter_from_below == False):
-            self.set_auto_mode(False)
-            self.output = 0
-        # If T_node is over the deadband, dont send power and reset hysteresis state
-        elif self.T_node > self.upper_db:
-            self.enter_from_below = False
-            self.output = 0
-        #'If T_node is under the deadband, send power with PID control and set hysteresis state
+
+        # If temperature is below dead band once, enable the PID boiler
         elif self.T_node < self.lower_db:
-            self.enter_from_below = True
+            if not self.isEnabled:
+                self.isEnabled = True
             if not self.auto_mode:
-                self.set_auto_mode(True, last_output=self.output)
+                self.set_auto_mode(True)
             if self.output < self.P_min:
-                self.output = self.P_min
-            # self.output = self.__call__(self.T_node)
-        # return output
+                self.output = self.P_min  # without this statement dip around lower dead band limit
 
 
 if __name__ == "__main__":
-    g = gasboiler(kp=1000, ki=0, kd=0, T_setpoint=20, T_node=15, T_amb=10, dead_band=2, P_max=10000, P_min=1500)
-    g.output_limits = (0, g.P_max)
-    # g.sample_time = 0.1
-    print(g.output)
+    g = GasBoiler(kp=1000, ki=0, kd=0,
+                  T_setpoint=20, T_node=15, T_amb=10,
+                  dead_band=2, P_max=10000, P_min=1500)
+    g.output_limits = (0, g.P_max)  # no offset: P_min = 0
+    # g.output_limits = (g.P_min, g.P_max)  # not what we want: P_min is offset for PID output
 
-    #testing Gasboiler class
-
-    tm = np.arange(0, 100, 0.1)
+    # testing Gasboiler class
+    g.current_step = 0.1
+    tm = np.arange(0, 20, 0.1)
     amplitude = 5 * np.sin(tm) + 18
     hysteresis = []
-    for i in amplitude:
-        g.T_node = i
+    state = []
+    for i in range(len(tm)):
+        g.T_node = amplitude[i]
         g.update()
         hysteresis.append(g.output)
-        # time.sleep(0.002)
-    result = np.vstack((amplitude, np.array(hysteresis)))
+        state.append(g.isEnabled)
 
-    fig, ax = plt.subplots(2,1)
-    ax[0].plot(tm, amplitude)
-    ax[1].plot(tm, hysteresis)
+    result = np.vstack((amplitude, np.array(hysteresis), state))
+
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(tm, amplitude, '.b-')
+    ax[1].plot(tm, hysteresis, '.b-')
     ax[0].grid(True)
     ax[1].grid(True)
     plt.show()
