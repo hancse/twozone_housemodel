86d03ca845ceb5b2b08f2f27e813a7396501f979
pjmvankan <pjmvankan@freedom.nl>
2021-08-24 13:42:26 +0200

    Documentation: added section on 2R2C_radiator model in House_model_references.pdf Added radiators.py for radiator equations according to several sources.

---
+++ housemodel/sourcesink/radiators.py
@@ -0,0 +1,33 @@
+
+# https://learn.openenergymonitor.org/sustainable-energy/building-energy-model/radiatormodel
+# https://www.buildingservicesindex.co.uk/entry/136540/AEL-Heating-Solutions-Ltd/How-to-calculate-the-delta-T-for-a-radiator/
+
+import matplotlib
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+def calc_corr_fact(delta_t):
+    dt_model = [5, 10, 15,20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
+    cf_model = [0.05, 0.123, 0.209, 0.304, 0.406, 0.515, 0.629, 0.748, 0.872, 1.0, 1.132, 1.267, 1.406, 1.549, 1.694]
+    f = interpolate.interp1d(dt_model, cf_model)
+    cf = f(delta_t)
+    return cf
+
+
+if __name__ == "__main__":
+    Delta_T = [20, 25, 30, 35, 40, 45, 50]
+    cf = [0.3, 0.41, 0.52, 0.63, 0.75, 0.87, 1.0]
+
+    Delta_T_2 = [5, 10, 15,20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
+    cf_2 = [0.05, 0.123, 0.209, 0.304, 0.406, 0.515, 0.629, 0.748, 0.872, 1.0, 1.132, 1.267, 1.406, 1.549, 1.694]
+
+    dt_test = 53
+    cf_test = calc_corr_fact(dt_test)
+    fig, ax = plt.subplots()
+    ax.plot(Delta_T, cf, '.b-')
+    ax.plot(Delta_T_2, cf_2, '.r-')
+    ax.plot(dt_test, cf_test, 'og')
+    ax.grid(True)
+    ax.set_ylim(0, 1.75)
+    plt.show()
