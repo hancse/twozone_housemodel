ecc5a371a1166bd81aa7414558ea5511ef28557a
pjmvankan <pjmvankan@freedom.nl>
2021-11-25 08:30:00 +0100

    Updated recipe for environment

--- conda_install.bat
+++ conda_install.bat
@@ -1,11 +1,14 @@
-call conda create -n test python=3.8
-call activate test
+call conda create -n futurefactory python=3.8
+call activate futurefactory
+
 call conda install matplotlib
 call conda install scipy
-call conda install jupyter notebook
 call conda install pandas
 call conda install openpyxl
+
+# call conda install jupyter notebook
 call conda install pyyaml
 call conda install holidays
+
 call conda list
 call conda info -e
