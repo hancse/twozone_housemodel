77b619c39221c09ceee4e47afb95f854ffaf0909
pjmvankan <pjmvankan@freedom.nl>
2023-01-25 18:23:57 +0100

    Cleaned up Simulation_for_companies_nodes_edges.py

--- Simulation_for_companies_nodes_edges.py
+++ Simulation_for_companies_nodes_edges.py
@@ -43,63 +43,13 @@ import matplotlib.pyplot as plt
 import logging
 
 logging.basicConfig()
-logger = logging.getLogger('HBNE')
+logger = logging.getLogger('COMP')
 # logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
 
 from pathlib import Path
 
 CONFIGDIR = Path(__file__).parent.absolute()
-
-
-def show(sim):
-    import matplotlib.pyplot as plt
-
-    plt.figure(figsize=(15, 5))  # key-value pair: no spaces
-    plt.plot(sim.t, sim.data[KEY_T_AIR], label='Tair')
-    plt.plot(sim.t, sim.data[KEY_T_WALL], label='Twall')
-    plt.plot(sim.t, sim.data[KEY_T_RADIATOR], label='Tradiator')
-    plt.plot(sim.t, sim.SP_sim, label='SP_Temperature')
-    plt.plot(sim.t, sim.T_outdoor_sim, label='Toutdoor')
-    plt.plot(sim.t, sim.Qinst, label='Qinst')
-    plt.legend(loc='best')
-    plt.title("Simulation2R2C_companies")
-    plt.show()
-
-
-def export_xl(sim):
-    # from housemodel.sourcesink.NEN5060 import run_qsun
-    # from housemodel.weather_solar.weatherdata import read_nen_weather_from_xl, NENdatehour2datetime
-    # from scipy.interpolate import interp1d
-
-    control_interval = sim.control_interval
-
-    # df_out = pd.DataFrame(data[0], columns=['Timestep'])
-    df_out = pd.DataFrame({'Timestep': sim.time_sim})
-    df_out['Outdoor temperature'] = sim.T_outdoor_sim
-    df_out['NEN5060_global'] = sim.data[KEY_SOLAR]
-    df_out['cloud_cover'] = sim.data[KEY_CLOUD]
-    df_out["Heating"] = sim.Qinst.tolist()
-    df_out['Setpoint'] = sim.SP_sim
-
-    num_links = len(sim.house_param["chains"][0]["links"])
-
-    for n in range(num_links):
-        nodename = sim.house_param['chains'][0]['links'][n]['Name']
-        df_out["T_{}".format(n)] = sim.solver.data[n + 1].tolist()
-        # df_out["Solar_{}".format(n)] = sim.Qsolar_sim[n, :]
-        if nodename == 'Internals':
-            df_out["Internal_{}".format(n)] = sim.data[KEY_Q_INTERNAL]
-
-    df_out['Tradiator'] = sim.data[KEY_T_RADIATOR].tolist()  # FIXME this is the same as T_2?
-
-    wb = Workbook()
-    ws = wb.active
-
-    for r in dataframe_to_rows(df_out, index=False):
-        ws.append(r)
-    # df_out.to_excel('tst.xlsx', index=False, startrow=10)
-    wb.save('tst_ML.xlsx')
 
 
 def main(show=False, xl=False):
@@ -119,34 +69,7 @@ def main(show=False, xl=False):
     h.boundaries_from_dict(param["boundaries"])  # function selects "outdoor" as ambient
     h.make_k_ext_and_add_ambient()  # initialize k_ext_mat and add diagonal elements
 
-    # read ALL edges in the system; select edges connecting nodes in taglist; create conductivity matrix
-    # h.edges_from_dict(param["edges"])
-    # h.fill_k(param["edges"])
-
-    # create empty q-vector (all zeros)
-    # h.make_empty_q_vec()
-    # the q_vec is made in the iteration, after all elements of T_outdoor have been read from NEN5060
-    # h.add_ambient_to_q()
-
     logger.info(f" \n\n C^-1: \n {h.c_inv_mat} \n K_ext: \n {h.k_ext_mat}, \n q_vec: \n {h.q_vec} \n")
-
-    # create StratifiedBuffer object
-    b = StratifiedBuffer("Buffervessel")
-    section = param["Buffer"]
-    b.nodes_from_dict(section["nodes"])
-    b.fill_c_inv()
-
-    b.boundaries_from_dict(param["boundaries"])  # function selects "indoor" as ambient
-    # b.add_fixed_to_k()  # crashes because Fixed node connects to node 2,3,4 and b.k_matrix has rank 3 (index 0,1,2)
-    b.make_k_ext_and_add_ambient()
-
-    # b.edges_from_dict(param['edges'])
-    # b.fill_k(param["edges"])
-
-    # b.make_empty_q_vec()
-    # b.add_ambient_to_q()
-
-    logger.info(f" \n\n C^-1: \n {b.c_inv_mat} \n K_ext: \n {b.k_ext_mat}, \n q_vec: \n {b.q_vec} \n")
 
     r = LinearRadiator("SimpleRadiator")
     section = param["Radiator"]
@@ -154,16 +77,7 @@ def main(show=False, xl=False):
     r.fill_c_inv()
     r.make_empty_k_ext_mat()
 
-    # r = Radiator(1.3)
-    # r.boundaries_from_dict(param["boundaries"])
-    # r.T_amb = 20.0  # = h.Tini
-
     logger.info(f" \n\n C^-1: \n {r.c_inv_mat} \n K_ext: \n {r.k_ext_mat}, \n q_vec: \n {r.q_vec} \n")
-
-    # Loading the radiator and buffervessel parameters
-    # Heat transfer coefficient of the radiator and het capacity
-    # UAradiator = house_param["chains"][0]["links"][2]["Conductance"]
-    # Crad =  house_param["chains"][0]["links"][2]["Capacity"]
 
     # create Totalsystem object and sort parts
     total = TotalSystem("HouseWithRadiator", [r, h])
@@ -183,29 +97,6 @@ def main(show=False, xl=False):
     total.make_empty_q_vec()
 
     logger.info(f" \n\n {total.c_inv_mat} \n\n {total.k_mat}, \n\n {total.q_vec} \n")
-
-    # calculate flow matrices and combine into f_mat_all
-    flows = []
-    for n in range(len(param['flows'])):
-        flows.append(Flow())
-        flows[n].flow_from_dict(param['flows'][n])
-        flows[n].make_Fmatrix(rank=total.k_mat.shape[0])
-
-    # combine F-matrices into matrix Fall
-    f_mat_all = np.zeros_like(flows[0].f_mat)
-    for n in range(len(flows)):
-        f_mat_all += flows[n].f_mat
-    # f_mat_all = np.add(flows[0].f_mat, flows[1].f_mat)
-    print(f_mat_all, "\n")
-
-    # remove matrix elements > 0 from Fall
-    f_mat_all = np.where(f_mat_all <= 0, f_mat_all, 0)
-    print(f_mat_all, "\n")
-
-    # create diagonal elements in Fall, so that som over each row is zero
-    row_sums = np.sum(f_mat_all, axis=1).tolist()
-    f_mat_all = f_mat_all - np.diag(np.array(row_sums), k=0)
-    print(f_mat_all, "\n")
 
     # read NEN5060 data from spreadsheet NEN5060-2018.xlsx into pandas DataFrame
     df_nen = read_nen_weather_from_xl()
@@ -248,7 +139,6 @@ def main(show=False, xl=False):
                                      param['internal']['t2'])
     Qint.values = Qint.values.flatten()
     Qint.values = Qint.values[0:days_sim * 24]
-    # Qinternal_sim = Qint[0:days_sim * 24]
 
     Toutdoor = PowerSource("T_outdoor")
     Toutdoor.values = df_nen.loc[:, 'temperatuur'].values
@@ -308,21 +198,24 @@ def main(show=False, xl=False):
 
     # if show=True, plot the results
     if show:
+        time_d = data[0]/(3600*24)
         plt.figure(figsize=(15, 5))  # key-value pair: no spaces
-        plt.plot(data[0], data[1], label='Tair')
-        plt.plot(data[0], data[2], label='Twall')
-        plt.plot(data[0], data[3], label='Tradiator')
-        plt.plot(time_sim, SP.values, label='SP_Temperature')
-        plt.plot(time_sim, Toutdoor.values, label='Toutdoor')
-        plt.plot(data[0], data[4], label='Qinst')
+        plt.plot(time_d, data[1], label='Tair')
+        plt.plot(time_d, data[2], label='Twall')
+        plt.plot(time_d, data[3], label='Tradiator')
+        plt.plot(time_sim/(3600*24), SP.values, label='SP_Temperature')
+        plt.plot(time_sim/(3600*24), Toutdoor.values, label='Toutdoor')
+        plt.plot(time_d, data[4], label='Qinst')
         plt.legend(loc='best')
         plt.title("Simulation2R2C_companies Nodes and Edges")
+        plt.xlabel("time [days]")
+        plt.ylabel("temperature [\xb0C]")
         plt.show()
 
     if xl:
         # df_out = pd.DataFrame(data[0], columns=['Timestep'])
         df_out = pd.DataFrame({'Timestep': data[0]})
-        df_out['Outdoor temperature'] = Toutdoor
+        df_out['Outdoor temperature'] = Toutdoor.values
         df_out['NEN5060_global'] = glob.values
         df_out['cloud_cover'] = cloud.values
         df_out["Heating"] = data[4].tolist()
@@ -358,15 +251,3 @@ if __name__ == "__main__":
     # compatible with MvdB, TN
     main(show=True, xl=False)  # temporary solution, recommended syntax
 
-    # compatible with ReneJ (run_for_companies.py)
-    # param = load_config(str(CONFIGDIR / "xl_for_2R2Chouse_buffer.yml"))
-    # param.setdefault('timing', {})
-    # house_param['timing']['days_sim'] = 100
-
-    # currentSolver = SolverForCompanies
-    # sim = Simulation2(param, currentSolver, SP=simple_thermostat(8, 23, 20, 17))
-
-    # sim.run()
-
-    # show(sim)
-    # export_xl(sim)
